45f3e5ba6206a100ab7eed0a19e09e712191bc88
Björn Hurling <bjoern.hurling@wissenswerft.net>
2014-08-08 15:19:14 +0200

    Upload APK to alpha track

--- src/main/groovy/de/triplet/gradle/play/PlayPublishTask.groovy
+++ src/main/groovy/de/triplet/gradle/play/PlayPublishTask.groovy
@@ -1,7 +1,11 @@
 package de.triplet.gradle.play
 
+import com.google.api.client.http.AbstractInputStreamContent
+import com.google.api.client.http.FileContent
 import com.google.api.services.androidpublisher.AndroidPublisher
+import com.google.api.services.androidpublisher.model.Apk
 import com.google.api.services.androidpublisher.model.AppEdit
+import com.google.api.services.androidpublisher.model.Track
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
@@ -29,7 +33,25 @@ class PlayPublishTask extends DefaultTask {
 
         final String editId = edit.getId();
 
+        final AbstractInputStreamContent apkFile =
+                new FileContent(AndroidPublisherHelper.MIME_TYPE_APK, inputFile);
 
+
+        AndroidPublisher.Edits.Apks.Upload uploadRequest = edits
+                .apks()
+                .upload(extension.applicationId, editId, apkFile);
+
+        Apk apk = uploadRequest.execute();
+
+        List<Integer> apkVersionCodes = new ArrayList<>();
+        apkVersionCodes.add(apk.getVersionCode());
+        AndroidPublisher.Edits.Tracks.Update updateTrackRequest = edits
+                .tracks()
+                .update(extension.applicationId, editId, "alpha", new Track().setVersionCodes(apkVersionCodes));
+        updateTrackRequest.execute();
+
+        AndroidPublisher.Edits.Commit commitRequest = edits.commit(extension.applicationId, editId);
+        commitRequest.execute();
     }
 
     void setExtension(PlayPublisherPluginExtension extension) {
