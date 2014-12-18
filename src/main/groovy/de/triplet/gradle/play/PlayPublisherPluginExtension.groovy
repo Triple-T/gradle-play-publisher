271a9cfcbb866ae3f313f2cfc32e472a48d0a4d2
Christian Becker <christian.becker@qnips.com>
2014-12-18 09:28:08 +0100

    Use Groovy Beans in our plugin extension

--- src/main/groovy/de/triplet/gradle/play/PlayPublisherPluginExtension.groovy
+++ src/main/groovy/de/triplet/gradle/play/PlayPublisherPluginExtension.groovy
@@ -2,37 +2,13 @@ package de.triplet.gradle.play
 
 class PlayPublisherPluginExtension {
 
-    private String serviceAccountEmail
+    String serviceAccountEmail
+
+    File pk12File
+
+    boolean uploadImages = false
 
     private String track = 'alpha'
-
-    private File pk12File
-
-    private boolean uploadImages = false
-
-    void setUploadImages(boolean uploadImages) {
-        this.uploadImages = uploadImages
-    }
-
-    boolean getUploadImages() {
-        return uploadImages
-    }
-
-    void setServiceAccountEmail(String email) {
-        serviceAccountEmail = email
-    }
-
-    def getServiceAccountEmail() {
-        return serviceAccountEmail
-    }
-
-    void setPk12File(File file) {
-        pk12File = file
-    }
-
-    def getPk12File() {
-        return pk12File
-    }
 
     void setTrack(String track) {
         if (!(track in ['alpha', 'beta', 'production'])) {
