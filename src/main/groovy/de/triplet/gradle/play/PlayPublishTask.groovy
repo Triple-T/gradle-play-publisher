61f1ac9780bdb60c2d986a925b9d527c1d00d483
Björn Hurling <hurling83@gmail.com>
2015-07-11 16:31:11 +0200

    Add some explanation to Google's applicationNotFound exception - #75

--- src/main/groovy/de/triplet/gradle/play/PlayPublishTask.groovy
+++ src/main/groovy/de/triplet/gradle/play/PlayPublishTask.groovy
@@ -1,6 +1,7 @@
 package de.triplet.gradle.play
 
 import com.android.build.gradle.api.ApplicationVariant
+import com.google.api.client.googleapis.json.GoogleJsonResponseException
 import com.google.api.services.androidpublisher.AndroidPublisher
 import com.google.api.services.androidpublisher.model.AppEdit
 import org.gradle.api.DefaultTask
@@ -29,9 +30,21 @@ class PlayPublishTask extends DefaultTask {
 
         // Create a new edit to make changes to your listing.
         AndroidPublisher.Edits.Insert editRequest = edits.insert(variant.applicationId, null /* no content yet */)
-        AppEdit edit = editRequest.execute()
 
-        editId = edit.getId()
+        try {
+            AppEdit edit = editRequest.execute()
+            editId = edit.getId()
+        } catch (GoogleJsonResponseException e) {
+
+            // The very first release has to be uploaded via the web interface.
+            // We add a little explanation to Google's exception.
+            if (e.message != null && e.message.contains("applicationNotFound")) {
+                throw new IllegalArgumentException("No application was found for the package name " + variant.applicationId + ". Is this the first release for this app? The first version has to be uploaded via the web interface.", e);
+            }
+
+            // Just rethrow everything else.
+            throw e;
+        }
     }
 
 }
