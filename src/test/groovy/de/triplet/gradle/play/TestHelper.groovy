58a77e60b5dcf95f2791923567a2302d0928fa6c
shiraji <isogai.shiraji@gmail.com>
2015-10-15 01:27:54 +0900

    Added a test case for no sigining config

--- src/test/groovy/de/triplet/gradle/play/TestHelper.groovy
+++ src/test/groovy/de/triplet/gradle/play/TestHelper.groovy
@@ -31,4 +31,28 @@ public class TestHelper {
 
         return project
     }
+
+    public static Project noSigningConfigProject() {
+        Project project = ProjectBuilder.builder().withProjectDir(FIXTURE_WORKING_DIR).build()
+        project.apply plugin: 'com.android.application'
+        project.apply plugin: 'com.github.triplet.play'
+        project.android {
+            compileSdkVersion 22
+            buildToolsVersion '22.0.1'
+
+            defaultConfig {
+                versionCode 1
+                versionName '1.0'
+                minSdkVersion 22
+                targetSdkVersion 22
+            }
+
+            buildTypes {
+                release {
+                }
+            }
+        }
+
+        return project
+    }
 }
