785068f838ba57cc7b2e1743374f69e238b9de2e
Roc Boronat <roc@fewlaps.com>
2016-03-11 13:59:49 +0100

    Testing ImageFileFilter with some tests, to avoid breaking it in the future

---
+++ src/test/groovy/de/triplet/gradle/play/ImageFileFilterTest.groovy
@@ -0,0 +1,76 @@
+package de.triplet.gradle.play
+
+import org.junit.Test
+
+class ImageFileFilterTest extends GroovyTestCase {
+
+    @Test
+    void testJpg_returnsTrue() {
+        ImageFileFilter filter = new ImageFileFilter()
+        File file = new File("banana.jpg");
+
+        boolean result = filter.accept(file);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void testPng_returnsTrue() {
+        ImageFileFilter filter = new ImageFileFilter()
+        File file = new File("banana.png");
+
+        boolean result = filter.accept(file);
+
+        assertTrue(result);
+    }
+
+    @Test
+    void testJpeg_returnsFalse() {
+        ImageFileFilter filter = new ImageFileFilter()
+        File file = new File("banana.jpeg");
+
+        boolean result = filter.accept(file);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void testGif_returnsFalse() {
+        ImageFileFilter filter = new ImageFileFilter()
+        File file = new File("banana.gif");
+
+        boolean result = filter.accept(file);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void testTiff_returnsFalse() {
+        ImageFileFilter filter = new ImageFileFilter()
+        File file = new File("banana.tiff");
+
+        boolean result = filter.accept(file);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void testBmp_returnsFalse() {
+        ImageFileFilter filter = new ImageFileFilter()
+        File file = new File("banana.bmp");
+
+        boolean result = filter.accept(file);
+
+        assertFalse(result);
+    }
+
+    @Test
+    void testSvg_returnsFalse() {
+        ImageFileFilter filter = new ImageFileFilter()
+        File file = new File("banana.svg");
+
+        boolean result = filter.accept(file);
+
+        assertFalse(result);
+    }
+}
