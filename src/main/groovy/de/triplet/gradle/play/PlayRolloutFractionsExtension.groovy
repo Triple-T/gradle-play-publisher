cefea6e492110992cf1e8652777f2d7853b980d2
Roc Boronat <roc@fewlaps.com>
2016-03-11 12:52:39 +0100

    Refactoring PlayRolloutFractions to meet the Open Closed Principle

--- src/main/groovy/de/triplet/gradle/play/PlayRolloutFractionsExtension.groovy
+++ src/main/groovy/de/triplet/gradle/play/PlayRolloutFractionsExtension.groovy
@@ -2,24 +2,14 @@ package de.triplet.gradle.play
 
 class PlayRolloutFractionsExtension {
 
-    def fractions = [0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1.0]
+    def fractions = [0.005d, 0.01d, 0.05d, 0.1d, 0.2d, 0.5d, 1.0d]
 
     double getNextUserFraction(double userFraction) {
-        switch (userFraction) {
-            case 0.005:
-                return 0.01;
-            case 0.01:
-                return 0.05;
-            case 0.05:
-                return 0.1;
-            case 0.1:
-                return 0.2;
-            case 0.2:
-                return 0.5;
-            case 0.5:
-                return 1.0;
-            default:
-                return 1.0;
+        if (userFraction == 1.0d) {
+            return 1.0d;
+        } else {
+            int userFractionIndex = fractions.indexOf(userFraction);
+            return fractions.get(userFractionIndex + 1);
         }
     }
 
