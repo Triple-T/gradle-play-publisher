8b95eb769af562047a8675f98d515c4fae3f02a4
Roc Boronat <roc@fewlaps.com>
2016-03-11 13:04:48 +0100

    Testing that the class returns it's expected behaviour and implementing a first solution

--- src/main/groovy/de/triplet/gradle/play/PlayRolloutFractionsExtension.groovy
+++ src/main/groovy/de/triplet/gradle/play/PlayRolloutFractionsExtension.groovy
@@ -5,7 +5,22 @@ class PlayRolloutFractionsExtension {
     def fractions = [0.005, 0.01, 0.05, 0.1, 0.2, 0.5, 1.0]
 
     double getNextUserFraction(double userFraction) {
-        return 0.01
+        switch (userFraction) {
+            case 0.005:
+                return 0.01;
+            case 0.01:
+                return 0.05;
+            case 0.05:
+                return 0.1;
+            case 0.1:
+                return 0.2;
+            case 0.2:
+                return 0.5;
+            case 0.5:
+                return 1.0;
+            default:
+                return 1.0;
+        }
     }
 
 }
--- src/test/groovy/de/triplet/gradle/play/PlayRolloutFractionsExtensionTest.groovy
+++ src/test/groovy/de/triplet/gradle/play/PlayRolloutFractionsExtensionTest.groovy
@@ -13,4 +13,65 @@ class PlayRolloutFractionsExtensionTest extends GroovyTestCase {
 
         assertEquals(0.01, nextUserFraction);
     }
+
+    @Test
+    void test_ShouldReturn005_whenInputIs001() {
+        PlayRolloutFractionsExtension extension = new PlayRolloutFractionsExtension();
+        double userFraction = 0.01;
+
+        double nextUserFraction = extension.getNextUserFraction(userFraction);
+
+        assertEquals(0.05, nextUserFraction);
+    }
+
+    @Test
+    void test_ShouldReturn01_whenInputIs005() {
+        PlayRolloutFractionsExtension extension = new PlayRolloutFractionsExtension();
+        double userFraction = 0.05;
+
+        double nextUserFraction = extension.getNextUserFraction(userFraction);
+
+        assertEquals(0.1, nextUserFraction);
+    }
+
+    @Test
+    void test_ShouldReturn02_whenInputIs01() {
+        PlayRolloutFractionsExtension extension = new PlayRolloutFractionsExtension();
+        double userFraction = 0.1;
+
+        double nextUserFraction = extension.getNextUserFraction(userFraction);
+
+        assertEquals(0.2, nextUserFraction);
+    }
+
+    @Test
+    void test_ShouldReturn05_whenInputIs02() {
+        PlayRolloutFractionsExtension extension = new PlayRolloutFractionsExtension();
+        double userFraction = 0.2;
+
+        double nextUserFraction = extension.getNextUserFraction(userFraction);
+
+        assertEquals(0.5, nextUserFraction);
+    }
+
+    @Test
+    void test_ShouldReturn1_whenInputIs05() {
+        PlayRolloutFractionsExtension extension = new PlayRolloutFractionsExtension();
+        double userFraction = 0.5;
+
+        double nextUserFraction = extension.getNextUserFraction(userFraction);
+
+        assertEquals(1, nextUserFraction);
+    }
+
+    @Test
+    void test_ShouldReturn1_whenInputIs1() {
+        PlayRolloutFractionsExtension extension = new PlayRolloutFractionsExtension();
+        double userFraction = 1;
+
+        double nextUserFraction = extension.getNextUserFraction(userFraction);
+
+        assertEquals(1, nextUserFraction);
+    }
+
 }
