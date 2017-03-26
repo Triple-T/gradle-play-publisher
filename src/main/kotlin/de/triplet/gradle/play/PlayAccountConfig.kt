b86d7e8de5385e946d8323210e0f0e954d1681a5
Christian Becker <christian.becker@qnips.com>
2017-03-26 19:06:53 +0200

    WIP: Try to convert PlayPublisherPlugin

--- src/main/kotlin/de/triplet/gradle/play/PlayAccountConfig.kt
+++ src/main/kotlin/de/triplet/gradle/play/PlayAccountConfig.kt
@@ -2,7 +2,7 @@ package de.triplet.gradle.play
 
 import java.io.File
 
-class PlayAccountConfig(val name: String) {
+open class PlayAccountConfig(val name: String) {
     var serviceAccountEmail: String? = null
 
     var pk12File: File? = null
