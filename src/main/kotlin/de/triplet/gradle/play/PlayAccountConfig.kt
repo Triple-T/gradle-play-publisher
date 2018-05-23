8a158f5b53ca912fbac358af444bbbd83a8a1bed
Charles Anderson <charles.s.anderson@gmail.com>
2018-05-23 08:15:27 -0400

    Switch to JDK instead of JRE for kotlin-stdlib as the JRE is deprecated. And change the String() to "" for clarity.

--- src/main/kotlin/de/triplet/gradle/play/PlayAccountConfig.kt
+++ src/main/kotlin/de/triplet/gradle/play/PlayAccountConfig.kt
@@ -2,7 +2,7 @@ package de.triplet.gradle.play
 
 import java.io.File
 
-open class PlayAccountConfig(var name: String = String()) {
+open class PlayAccountConfig(var name: String = "") {
     var serviceAccountEmail: String? = null
 
     var pk12File: File? = null
