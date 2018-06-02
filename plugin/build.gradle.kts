eb33b3bb298789fe5d29dfd8ae641ffbf8ba779a
Alex Saveau <saveau.alexandre@gmail.com>
2018-06-01 22:44:43 -0700

    Refactor modules and otherwise improve Gradle build
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm")
+    id("groovy")
+    id("java-gradle-plugin")
+}
+
+dependencies {
+    implementation("com.android.tools.build:gradle:3.0.1")
+    implementation("com.google.apis:google-api-services-androidpublisher:v2-rev77-1.23.0") {
+        exclude("com.google.guava", "guava-jdk5") // Remove when upgrading to AGP 3.1+
+    }
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test:$kotlinVersion")
+    testImplementation("junit:junit:4.12")
+    testImplementation("org.mockito:mockito-core:1.10.19")
+    testImplementation("org.assertj:assertj-core:3.6.2")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+gradlePlugin {
+    plugins {
+        create("play") {
+            id = "com.github.triplet.play"
+            implementationClass = "de.triplet.gradle.play.PlayPublisherPlugin"
+        }
+    }
+}
