708f021d701089c5ee9365752dfaed34b1b5804c
Charles Anderson <charles.s.anderson@gmail.com>
2018-06-03 13:36:34 -0400

    Add compatibility check to the `releaseStatus` and `track` options, per @bhurling's comments about not breaking backwards compatibility. It also provides feedback if somebody tries to set incompatible options. Tests included.

--- plugin/src/main/kotlin/de/triplet/gradle/play/PlayPublisherExtension.kt
+++ plugin/src/main/kotlin/de/triplet/gradle/play/PlayPublisherExtension.kt
@@ -5,6 +5,26 @@ import de.triplet.gradle.play.internal.ReleaseStatus
 import de.triplet.gradle.play.internal.TrackType
 
 open class PlayPublisherExtension : AccountConfig by PlayAccountConfigExtension() {
+    /**
+     * Used to give feedback for users setting incompatible status/track combos.
+     */
+    private var releaseTrackSet = false
+
+    /**
+     * Release statuses that are compatible with a [releaseStatus] of `rollout`
+     */
+    private var rolloutStatuses = listOf(ReleaseStatus.INPROGRESS, ReleaseStatus.HALTED)
+
+    /**
+     * Check the compatibility of [track] and [releaseStatus]
+     * For reference: [https://developers.google.com/android-publisher/api-ref/edits/tracks]
+     */
+    private fun checkTrackCompatibility() {
+        if ((_track != TrackType.ROLLOUT && rolloutStatuses.contains(_releaseStatus))
+            || (_track == TrackType.ROLLOUT && !rolloutStatuses.contains(_releaseStatus)))
+            throw IllegalArgumentException("Incompatible track and releaseStatus specified.")
+    }
+
     internal var _track = TrackType.INTERNAL
     /**
      * Specify the track in which to upload your app. May be one of internal, alpha, beta, rollout,
@@ -16,6 +36,11 @@ open class PlayPublisherExtension : AccountConfig by PlayAccountConfigExtension(
             _track = requireNotNull(TrackType.values().find { it.name.equals(value, true) }) {
                 "Track must be one of ${TrackType.values().joinToString { "'${it.publishedName}'" }}"
             }
+            if (releaseTrackSet)
+                checkTrackCompatibility()
+            else if (_track == TrackType.ROLLOUT)
+                _releaseStatus = ReleaseStatus.INPROGRESS
+            releaseTrackSet = true
         }
     /**
      * Choose whether or not to untrack superseded versions automatically. See
@@ -51,5 +76,10 @@ open class PlayPublisherExtension : AccountConfig by PlayAccountConfigExtension(
             _releaseStatus = requireNotNull(ReleaseStatus.values().find { it.name.equals(value, true) }) {
                 "Release Status must be one of ${ReleaseStatus.values().joinToString { "'${it.status}'" }}"
             }
+            if (releaseTrackSet)
+                checkTrackCompatibility()
+            else if (rolloutStatuses.contains(_releaseStatus))
+                _track = TrackType.ROLLOUT
+            releaseTrackSet = true
         }
 }
