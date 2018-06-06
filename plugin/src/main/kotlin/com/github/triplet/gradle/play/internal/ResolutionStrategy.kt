ef4f1429a2a40044fa29ef52eb1ed32c9069abf5
Alex Saveau <saveau.alexandre@gmail.com>
2018-06-06 09:53:01 -0700

    Internal
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- plugin/src/main/kotlin/com/github/triplet/gradle/play/internal/ResolutionStrategy.kt
+++ plugin/src/main/kotlin/com/github/triplet/gradle/play/internal/ResolutionStrategy.kt
@@ -1,6 +1,6 @@
 package com.github.triplet.gradle.play.internal
 
-enum class ResolutionStrategy(val publishedName: String) {
+internal enum class ResolutionStrategy(val publishedName: String) {
     AUTO("auto"),
     FAIL("fail"),
     IGNORE("ignore")
