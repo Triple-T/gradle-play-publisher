5aed2c7e6e51902a58340f8ac9e435c4646ff2b3
Alex Saveau <saveau.alexandre@gmail.com>
2018-06-08 19:44:55 -0700

    Progress
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- plugin/src/main/kotlin/com/github/triplet/gradle/play/internal/Progress.kt
+++ plugin/src/main/kotlin/com/github/triplet/gradle/play/internal/Progress.kt
@@ -12,8 +12,12 @@ internal fun <T> AndroidPublisherRequest<T>.trackProgress(
     val uploader = mediaHttpUploader ?: return this
     uploader.chunkSize = 4 * MediaHttpUploader.MINIMUM_CHUNK_SIZE
     uploader.setProgressListener {
-        if (it.uploadState == MediaHttpUploader.UploadState.MEDIA_IN_PROGRESS) {
-            logger.progress("Uploading $thing: ${(it.progress * 100).roundToInt()}% complete")
+        @Suppress("NON_EXHAUSTIVE_WHEN")
+        when (it.uploadState) {
+            MediaHttpUploader.UploadState.MEDIA_IN_PROGRESS ->
+                logger.progress("Uploading $thing: ${(it.progress * 100).roundToInt()}% complete")
+            MediaHttpUploader.UploadState.MEDIA_COMPLETE ->
+                logger.progress("${thing.capitalize()} upload complete")
         }
     }
     return this
