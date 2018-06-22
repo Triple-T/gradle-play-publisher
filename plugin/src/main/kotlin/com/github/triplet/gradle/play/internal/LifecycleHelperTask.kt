77578ed272711d426d7790c81e923a20f623de72
Alex Saveau <saveau.alexandre@gmail.com>
2018-06-22 00:40:44 -0700

    Add missing annotation
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- plugin/src/main/kotlin/com/github/triplet/gradle/play/internal/LifecycleHelperTask.kt
+++ plugin/src/main/kotlin/com/github/triplet/gradle/play/internal/LifecycleHelperTask.kt
@@ -2,8 +2,9 @@ package com.github.triplet.gradle.play.internal
 
 import com.github.triplet.gradle.play.PlayPublisherExtension
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Nested
 import javax.inject.Inject
 
 open class LifecycleHelperTask @Inject constructor(
-        override val extension: PlayPublisherExtension
+        @get:Nested override val extension: PlayPublisherExtension
 ) : DefaultTask(), ExtensionOptions
