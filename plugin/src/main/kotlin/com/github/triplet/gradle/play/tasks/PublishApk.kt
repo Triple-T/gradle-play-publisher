be01ecafbd84188cb313fb512703ccdb3a9f70aa
Alex Saveau <saveau.alexandre@gmail.com>
2018-09-02 12:19:47 -0700

    Don't upload universal APK if splits are provided
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- plugin/src/main/kotlin/com/github/triplet/gradle/play/tasks/PublishApk.kt
+++ plugin/src/main/kotlin/com/github/triplet/gradle/play/tasks/PublishApk.kt
@@ -1,5 +1,6 @@
 package com.github.triplet.gradle.play.tasks
 
+import com.android.build.VariantOutput.OutputType
 import com.android.build.gradle.api.ApkVariantOutput
 import com.github.triplet.gradle.play.internal.PlayPublishPackageBase
 import com.github.triplet.gradle.play.internal.playPath
@@ -24,7 +25,9 @@ open class PublishApk : PlayPublishPackageBase() {
     @get:InputFiles
     internal val inputApks by lazy {
         // TODO: If we take a customizable folder, we can fix #233, #227
-        variant.outputs.filterIsInstance<ApkVariantOutput>().map { it.outputFile }
+        variant.outputs.filterIsInstance<ApkVariantOutput>().filter {
+            OutputType.valueOf(it.outputType) == OutputType.MAIN || it.filters.isNotEmpty()
+        }.map { it.outputFile }
     }
     @Suppress("MemberVisibilityCanBePrivate", "unused") // Used by Gradle
     @get:PathSensitive(PathSensitivity.RELATIVE)
