ab28ada849efa01abc8543b15c1f4aa9c5c88fcc
sergey <sergey.chelombitko@gmail.com>
2018-09-26 01:07:27 +0300

    Fix possible NPE in bootstrapProducts

--- plugin/src/main/kotlin/com/github/triplet/gradle/play/tasks/Bootstrap.kt
+++ plugin/src/main/kotlin/com/github/triplet/gradle/play/tasks/Bootstrap.kt
@@ -120,7 +120,7 @@ open class Bootstrap : PlayPublishTaskBase() {
 
     private fun bootstrapProducts() {
         progressLogger.progress("Downloading in-app products")
-        publisher.inappproducts().list(variant.applicationId).execute().inappproduct.forEach {
+        publisher.inappproducts().list(variant.applicationId).execute().inappproduct?.forEach {
             JacksonFactory.getDefaultInstance()
                     .toPrettyString(it)
                     .write(srcDir, "$PRODUCTS_PATH/${it.sku}.json")
