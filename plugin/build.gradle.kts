6704618803b006dedf0f5da690e12d1c9d540057
Bjoern Hurling <hurling83@gmail.com>
2018-11-15 15:24:53 +0100

    Publish to Gradle Plugin Portal

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,11 +1,11 @@
-import org.codehaus.groovy.runtime.InvokerHelper
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmCompile
 
 plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
+    `maven-publish`
     groovy
-    id("com.vanniktech.maven.publish")
+    id("com.gradle.plugin-publish") version "0.10.0"
 }
 
 dependencies {
@@ -33,29 +33,104 @@ tasks.withType<KotlinJvmCompile> {
     }
 }
 
+group = "com.github.triplet.gradle"
+version = "2.0.0-beta1"
+
 gradlePlugin {
-    plugins {
-        create("play") {
-            id = "com.github.triplet.play"
-            implementationClass = "com.github.triplet.gradle.play.PlayPublisherPlugin"
+    plugins.create("play") {
+        id = "com.github.triplet.play"
+        displayName = "Gradle Play Publisher"
+        description = "Gradle Play Publisher allows you to upload your App Bundle or APK " +
+                "and other app details to the Google Play Store."
+        implementationClass = "com.github.triplet.gradle.play.PlayPublisherPlugin"
+    }
+}
+
+pluginBundle {
+    website = "https://github.com/Triple-T/gradle-play-publisher"
+    vcsUrl = "https://github.com/Triple-T/gradle-play-publisher"
+    tags = listOf("android", "google-play")
+
+    mavenCoordinates {
+        groupId = project.group as String
+        artifactId = "play-publisher"
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "Snapshots"
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+
+            credentials {
+                username = project.findProperty("SONATYPE_NEXUS_USERNAME")?.toString()
+                password = project.findProperty("SONATYPE_NEXUS_PASSWORD")?.toString()
+            }
         }
     }
 }
 
-afterEvaluate {
-    fun PomFilterContainer.removeTestDependencies() {
-        pom.whenConfigured {
-            dependencies.removeIf {
-                // Stolen from JetBrains' own sample at
-                // https://github.com/JetBrains/kotlin/blob/v1.2.50/buildSrc/src/main/kotlin/plugins/PublishedKotlinModule.kt#L86
-                InvokerHelper.getMetaClass(it).getProperty(it, "scope") == "test"
-            }
-        }
-    }
+val sourcesJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets["main"].allSource)
+    dependsOn(tasks["classes"])
+}
 
-    tasks.withType<Upload> {
-        repositories.forEach {
-            (it as? PomFilterContainer)?.removeTestDependencies()
+afterEvaluate {
+    publishing.publications.named<MavenPublication>("pluginMaven") {
+        artifactId = "play-publisher"
+        artifact(sourcesJar.get())
+
+        pom {
+            name.set("Google Play Publisher")
+            description.set("Gradle Play Publisher is a plugin that allows you to upload your " +
+                                    "App Bundle or APK and other app details to the " +
+                                    "Google Play Store.")
+            url.set("https://github.com/Triple-T/gradle-play-publisher")
+
+            licenses {
+                license {
+                    name.set("The MIT License (MIT)")
+                    url.set("http://opensource.org/licenses/MIT")
+                    distribution.set("repo")
+                }
+            }
+
+            developers {
+                developer {
+                    id.set("bhurling")
+                    name.set("Björn Hurling")
+                    roles.set(listOf("Owner"))
+                    timezone.set("+2")
+                }
+                developer {
+                    id.set("SUPERCILEX")
+                    name.set("Alex Saveau")
+                    email.set("saveau.alexandre@gmail.com")
+                    roles.set(listOf("Developer"))
+                    timezone.set("-8")
+                }
+                developer {
+                    id.set("ChristianBecker")
+                    name.set("Christian Becker")
+                    email.set("christian.becker.1987@gmail.com")
+                    roles.set(listOf("Developer"))
+                    timezone.set("+2")
+                }
+                developer {
+                    id.set("gtcompscientist")
+                    name.set("Charles Anderson")
+                    roles.set(listOf("Developer"))
+                    timezone.set("-8")
+                }
+            }
+
+            scm {
+                connection.set("scm:git@github.com:Triple-T/gradle-play-publisher.git")
+                developerConnection.set("scm:git@github.com:Triple-T/gradle-play-publisher.git")
+                url.set("https://github.com/Triple-T/gradle-play-publisher")
+            }
         }
     }
 }
@@ -64,8 +139,6 @@ tasks.withType<Test> {
     // Our integration tests need a fully compiled jar
     dependsOn("assemble")
 
-    // Those tests also need to know which version code was built
-    project.property("VERSION_NAME")?.let {
-        systemProperty("VERSION_NAME", it)
-    }
+    // Those tests also need to know which version was built
+    systemProperty("VERSION_NAME", version)
 }
