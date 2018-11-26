668440acc4b34c379ab1d4461c57aea83a095c19
Felix <mail@nicidienase.de>
2018-11-26 15:31:29 +0100

    use truststore/password defined in javax.net.ssl.trustStore(Password) (Issue #455)

--- plugin/src/main/kotlin/com/github/triplet/gradle/play/internal/Constants.kt
+++ plugin/src/main/kotlin/com/github/triplet/gradle/play/internal/Constants.kt
@@ -1,7 +1,11 @@
 package com.github.triplet.gradle.play.internal
 
+import com.google.api.client.googleapis.GoogleUtils
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport
 import com.google.api.client.http.javanet.NetHttpTransport
+import java.io.FileInputStream
+import java.security.KeyStore
+
 
 internal const val PLUGIN_NAME = "gradle-play-publisher"
 internal const val PLUGIN_GROUP = "Publishing"
@@ -15,4 +19,19 @@ internal const val RESOURCES_OUTPUT_PATH = "generated/gpp"
 
 internal const val MIME_TYPE_STREAM = "application/octet-stream"
 
-internal val transport: NetHttpTransport by lazy { GoogleNetHttpTransport.newTrustedTransport() }
+internal val transport: NetHttpTransport by lazy { buildTransport() }
+
+internal fun buildTransport(): NetHttpTransport {
+    val trustStore: String? = System.getProperty("javax.net.ssl.trustStore", null)
+    val trustStorePassword: String? = System.getProperty("javax.net.ssl.trustStorePassword", null)
+
+    return if (trustStore != null && trustStorePassword != null) {
+
+        val ks = KeyStore.getInstance(KeyStore.getDefaultType())
+        FileInputStream(trustStore).use { fis -> ks.load(fis, trustStorePassword.toCharArray()) }
+
+        NetHttpTransport.Builder().trustCertificates(ks).build()
+    } else {
+        GoogleNetHttpTransport.newTrustedTransport()
+    }
+}
