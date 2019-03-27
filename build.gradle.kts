bf7dacba5bd793c10d2f785c4a7c111cf3bb653d
Alex Saveau <saveau.alexandre@gmail.com>
2019-03-27 13:31:47 -0700

    Move snapshot publishing bash script to Gradle (#525)
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,17 @@ tasks.register<Delete>("clean") {
     delete("build")
 }
 
+tasks.register("ciBuild") {
+    val isMaster = System.getenv("TRAVIS_BRANCH") == "master"
+    val isPr = System.getenv("TRAVIS_PULL_REQUEST") ?: "false" != "false"
+
+    if (isMaster && !isPr) { // Release build
+        dependsOn(":plugin:build", ":plugin:publish")
+    } else {
+        dependsOn(":plugin:check")
+    }
+}
+
 allprojects {
     repositories {
         google()
