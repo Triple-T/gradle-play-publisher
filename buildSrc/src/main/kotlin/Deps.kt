03fc28afd8960173df10ad222d3a22bc88d7ee9e
Alex Saveau <saveau.alexandre@gmail.com>
2019-09-27 00:00:32 -0700

    Fix a bunch of compatibility issues (#711)
    Signed-off-by: Alex Saveau <saveau.alexandre@gmail.com>

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,5 +1,4 @@
 import org.gradle.api.artifacts.dsl.RepositoryHandler
-import org.gradle.kotlin.dsl.maven
 
 fun RepositoryHandler.deps() {
     google().content {
@@ -12,19 +11,15 @@ fun RepositoryHandler.deps() {
         includeGroup("io.fabric.sdk.android")
     }
 
-    maven("https://dl.bintray.com/kotlin/kotlin-dev/").content {
-        includeGroup("org.jetbrains.kotlin")
-    }
-
     jcenter()
 }
 
 object Config {
     object Libs {
         object All {
-            const val agp = "com.android.tools.build:gradle:3.6.0-alpha11"
+            const val agp = "com.android.tools.build:gradle:3.6.0-alpha12"
             const val ap =
-                    "com.google.apis:google-api-services-androidpublisher:v3-rev20190818-1.30.1"
+                    "com.google.apis:google-api-services-androidpublisher:v3-rev20190910-1.30.1"
 
             const val truth = "com.google.truth:truth:1.0"
             const val junit = "junit:junit:4.12"
