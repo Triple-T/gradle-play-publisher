c4503dd7eb4a443702ba11f7de5b44b3c55d5c64
Francesco Cervone <francesco.cervone@prima.it>
2023-06-26 15:34:21 +0200

    Update Google API Client to 2.2.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -39,7 +39,7 @@ dependencyResolutionManagement {
             version("agp", "8.0.1")
             version("agp-tools", "31.0.1")
             version("android-publisher", "v3-rev20211021-1.32.1")
-            version("api-client", "1.32.2")
+            version("api-client", "2.2.0")
             version("http-client", "1.40.1")
             version("http-auth", "1.2.2")
             version("guava", "31.0.1-jre")
